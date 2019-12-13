3bccdc70891c7f1cbafbfd5a9caa86662bb6dcd7
Tripple-A <biodun9@gmail.com>
2019-12-13 16:56:17 +0100

    initialized rails for the landing page

---
+++ app/controllers/landing_page_controller.rb
@@ -0,0 +1,7 @@
+class LandingPageController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'landing_page/home'
+  get 'landing_page/contact'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
