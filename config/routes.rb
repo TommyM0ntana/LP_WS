05f9f3073766dda4063860585569f36a7bebf9a0
Tripple-A <biodun9@gmail.com>
2019-12-13 23:05:03 +0100

    added the partail for the contact_form

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'landing_page#contact'
   root 'landing_page#home'
   post '/contact' , to: 'landing_page#create'
+  get '/home' , to: 'landing_page#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
