02e21c01a9789f232be6b58d8abe1c3251f8d859
sarah <sarachamorro@gmail.com>
2019-12-13 13:57:07 -0300

    Change /landing_page/contact path to /contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'landing_page/home'
-  get 'landing_page/contact'
+  get '/contact', to: 'landing_page#contact'
   root 'landing_page#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
