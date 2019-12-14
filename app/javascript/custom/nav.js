ff628a9f34702013578fc5f785e0603647a1ad08
sarah <sarachamorro@gmail.com>
2019-12-14 06:07:18 -0300

    add style to navbar

---
+++ app/javascript/custom/nav.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  console.log(document.getElementById("navbar"))
+
+  let prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+  } 
+}
